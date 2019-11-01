15d14a78cca135fbf85d0020584eb47a24408390
Sam Thurman <sam.bbmgmt@gmail.com>
2019-11-01 13:11:21 -0700

    branch 'question2' working now, question 2 complete, test pass

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ Write a function called multiply() that takes in two numbers as arguments and re
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
-// Write your code here
+// Write your code here.
 function multiply(a, b) { //eslint-disable-line
 var myArray = [];
 var productAnswer = a * b;
