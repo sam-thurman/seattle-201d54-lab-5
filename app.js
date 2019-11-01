4d8170f709a6ab24768175f9b8cadfc1e65a7e9a
Sam Thurman <sam.bbmgmt@gmail.com>
2019-11-01 13:14:32 -0700

    lets try it

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ Write a function called multiply() that takes in two numbers as arguments and re
 
 "The product of 5 and 9 is 45."
 
+
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here.
